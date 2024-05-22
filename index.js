da13b684cae6c926b3769407faee93287f3e1cec
Stevo <stevogk@gmail.com>
2024-05-22 17:37:05 +0200

    Create a server and listen to it

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
 /*
- * Title: Basic Node Example
- * Description: Simple file that declares a few functions and invokes them.
- * Author: Leslie Lewis
- * Date: 10/24/17
- *
- */
-
+* Primary file for the API
+*
+*/
 
 // Dependencies
-var mathLib = require('./lib/math');
-var jokesLib = require('./lib/jokes');
+var http = require('http');
+
+// The server should responde to all request with a string
+var server = http.createServer(function(req,res){
+    res.end('Hello World\n')
+});
 
 
-// App object
-var app = {};
-
-
-// Configuration
-app.config = {
-    'timeBetweenJokes' : 1000
-};
-
-
-// Function that prints a random joke
-app.printAJoke = function(){
-
-    // Get all the jokes
-    var allJokes = jokesLib.allJokes();
-
-    // Get the length of the jokes
-    var numberOfJokes = allJokes.length;
-
-    // Pick a random number between 1 and the number of jokes
-    var randomNumber = mathLib.getRandomNumber(1,numberOfJokes);
-
-    // Get the joke at that position in the array (minus one)
-    var selectedJoke = allJokes[randomNumber - 1];
-
-    // Send the joke to the console
-    console.log(selectedJoke);
-};
-
-
-// Function that loops indefinitely, calling the printAJoke function as it goes
-app.indefiniteLoop = function(){
-
-    // Create the interval, using the config variable defined above
-    setInterval(app.printAJoke,app.config.timeBetweenJokes);
-};
-
-
-// Invoke the loop
-app.indefiniteLoop();
+// Start the server, and ahve it listen on port 3000
+server.listen(3000,function(){
+    console.log('The server is listening on port 3000 now');
+})
